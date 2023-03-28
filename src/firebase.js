41b1de6414a032602ce99a882e89bdd3705e1b18
Charlyboy1993 <cjaramilloportilla@gmail.com>
2023-03-28 01:25:35 -0500

    Integrating Firebase

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from 'firebase/auth';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC51Ls0fyQOLUmpmphaNMDkRsj6zMHc7sU",
+  authDomain: "linkedin-55435.firebaseapp.com",
+  projectId: "linkedin-55435",
+  storageBucket: "linkedin-55435.appspot.com",
+  messagingSenderId: "245980652397",
+  appId: "1:245980652397:web:486e74442ab326fa957064"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
+
+
+export { db, auth };
